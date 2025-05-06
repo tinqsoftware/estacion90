fd7b91e7cc8a5990611ee6381500d542bd63607c
AaronAP1 <aaalenort@gmail.com>
2025-05-06 09:12:55 -0500

    cambios_FaltaDinamico

--- app/Http/Controllers/PlaneacionMenuController.php
+++ app/Http/Controllers/PlaneacionMenuController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PlaneacionMenuController extends Controller
 {
-    //
+    public function menusemanal()
+    {
+        return redirect()->route('menu.menu_semana');
+    }
 }
