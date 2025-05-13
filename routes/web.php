5d44f0f57ebbc31c1d0e0945d38268d382c52068
AaronAP1 <aaalenort@gmail.com>
2025-05-13 08:09:22 -0500

    SeguridadWeb

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Inicio;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\PlaneacionMenuController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 Route::get('/', function () {
@@ -14,44 +15,41 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+// Public routes
 Route::get('/', [Inicio::class, 'inicio']);
 
-//Productos
+// Protected routes - requires authentication
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
+    //Productos
+    Route::get('/productos', [ProductoController::class, 'productos_tab'])->name('productos_tab');
+    Route::post('/productos/crear', [ProductoController::class, 'store'])->name('productos.store');
+    Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
+    Route::get('/productos/{id}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
+    Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
+    Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
 
-Route::get('/productos', [ProductoController::class, 'productos_tab'])->name('productos_tab');
-Route::post('/productos/crear', [ProductoController::class, 'store'])->name('productos.store');
-Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
-Route::get('/productos/{id}/editar', [ProductoController::class, 'edit'])->name('productos.edit');
-Route::put('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
-Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
+    //MenuSemanal
+    Route::get('/menuSemanal', [PlaneacionMenuController::class, 'menusemanal'])->name('menu.menu_semana');
+    Route::get('/api/menusemana', [PlaneacionMenuController::class, 'getMenuSemanal']);
+    Route::get('/api/calendario', [PlaneacionMenuController::class, 'getMesCalendario']);
+    Route::get('/api/calendar-month', [PlaneacionMenuController::class, 'getMesCalendario']);
+    Route::get('/menusemana/agregar/{fecha?}', [PlaneacionMenuController::class, 'agregar']);
+    Route::post('/api/menu/registrar', [PlaneacionMenuController::class, 'registrarMenu']);
+    Route::delete('/api/menu/eliminar', [PlaneacionMenuController::class, 'eliminarMenu']);
 
+    //Usuarios
+    Route::get('/usuarios', [UsuarioController::class, 'prin']);
+    Route::post('/usuarios/guardar', [UsuarioController::class, 'store']);
+    Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+    Route::get('/usuarios/{id}/edit', [UsuarioController::class, 'edit']);
+    Route::put('/usuarios/guardar/{id}', [UsuarioController::class, 'update']);
+    Route::delete('/usuarios/eliminar/{id}', [UsuarioController::class, 'destroy']);
+    Route::post('/usuarios/{id}/reset-password', [UsuarioController::class, 'resetPassword']);
 
-//MenuSemanal
-Route::get('/menuSemanal', [PlaneacionMenuController::class, 'menusemanal'])->name('menu.menu_semana');
-Route::get('/api/menusemana', [PlaneacionMenuController::class, 'getMenuSemanal']);
-Route::get('/api/calendario', [PlaneacionMenuController::class, 'getMesCalendario']);
-Route::get('/api/calendar-month', [PlaneacionMenuController::class, 'getMesCalendario']);
-
-
-Route::get('/menusemana/agregar/{fecha?}', [PlaneacionMenuController::class, 'agregar']);
-Route::post('/api/menu/registrar', [PlaneacionMenuController::class, 'registrarMenu']);
-Route::delete('/api/menu/eliminar', [PlaneacionMenuController::class, 'eliminarMenu']);
-
-
-//Usuarios
-
-Route::get('/usuarios', [UsuarioController::class, 'prin']);
-Route::post('/usuarios/guardar', [UsuarioController::class, 'store']);
-Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
-Route::get('/usuarios/{id}/edit', [UsuarioController::class, 'edit']);
-Route::put('/usuarios/guardar/{id}', [UsuarioController::class, 'update']);
-Route::delete('/usuarios/eliminar/{id}', [UsuarioController::class, 'destroy']);
-Route::post('/usuarios/{id}/reset-password', [UsuarioController::class, 'resetPassword']);
-
-
-//cambios Contraseña
-Route::post('/user/reset-password', [UsuarioController::class, 'resetPasswordLOGIN'])->name('user.reset-password');
-Route::get('/cambiarclave', [UsuarioController::class, 'showChangePasswordForm'])->name('password.change');
-Route::post('/cambiar-password', [UsuarioController::class, 'changePassword'])->name('password.change.submit');
+    //cambios Contraseña
+    Route::post('/user/reset-password', [UsuarioController::class, 'resetPasswordLOGIN'])->name('user.reset-password');
+    Route::get('/cambiarclave', [UsuarioController::class, 'showChangePasswordForm'])->name('password.change');
+    Route::post('/cambiar-password', [UsuarioController::class, 'changePassword'])->name('password.change.submit');
+});
